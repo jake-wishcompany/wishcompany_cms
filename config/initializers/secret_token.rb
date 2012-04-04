862ca76b3c6490ead737cb75dd9afac1306f268c
jake-wishcompany <jake@wishcompany.net>
2012-04-04 18:02:05 +0900

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishCms::Application.config.secret_token = 'e68749d7c25dc2b74766226ab9e0e3101cb3def8242fa45548dadaf5e3f4a7fb9393ad0233901cd722e960f5ab067f7f0e302a6677ca860f11a42e54e20ab08c'
